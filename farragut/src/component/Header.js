fbbbb96d1f3e526028f677cf73af07903e41f447
ankushgour11 <ankush.gour@techment.com>
2023-01-20 12:11:45 +0530

    Commit for sidebar and dashboard UI changes

--- farragut/src/component/Header.js
+++ farragut/src/component/Header.js
@@ -5,6 +5,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import '../assets/css/header.css'
 
 const Header = ({ header }) => {
 
@@ -13,7 +14,7 @@ const Header = ({ header }) => {
   return (
     <div className="container-fluid header-val1 my-3">
       <div className="row d-flex align-items-center">
-        <div className="col-md-6">
+        <div className="col-6 header-menu-title">
           {!broken && (/* <button onClick={() => collapseSidebar()} className="btn p-0 btn-toggle">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -24,7 +25,7 @@ const Header = ({ header }) => {
           <div>
             {broken && (
               <button className="btn p-0 btn-toggle" onClick={() => toggleSidebar()}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
               </button>
@@ -33,9 +34,9 @@ const Header = ({ header }) => {
         </main>
           <div><h2>{header}</h2></div>
         </div>
-        <div className="col-md-6 d-flex justify-content-end">
+        <div className="col-6 d-flex justify-content-end">
           <NavDropdown
-              id="nav-dropdown-dark-example"
+              id="nav-user-profile-drodown"
               title="Mahfuzul Nabil"
               className="user-profile-drodown-header"
               align="end"
